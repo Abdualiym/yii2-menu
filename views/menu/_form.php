f9504b1a7bb02c6deac72d8d19d11fb0cebba66c
TermiT <web4@ivc.tshtt.uz>
2018-03-06 16:31:22 +0500

    fixed menu

--- views/menu/_form.php
+++ views/menu/_form.php
@@ -4,31 +4,47 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use abdualiym\menu\entities\Menu;
 use abdualiym\languageClass\Language;
-use yii\helpers\ArrayHelper;
 use yii\helpers\Json;
 use abdualiym\text\forms\TextForm;
 
 
 $langList = Language::langList(Yii::$app->params['languages']);
 $menuTypes = Menu::getMenuTypes();
-$translate = '';
-$typeHelper = '';
 $textForm = new TextForm();
 $categoriesList = $textForm->categoriesList();
 $textsList = $textForm->textsList();
 if (!empty($menu)) {
     $translate = $menu->translate;
     $typeHelper = $menu->type_helper;
+}else{
+    $translate = '';
+    $typeHelper = '';
 }
 
+
 /* @var $this yii\web\View */
-/* @var $model domain\modules\menu\forms\menu\MenuForm */
-/* @var $menu domain\modules\menu\entities\Menu */
+/* @var $model abdualiym\menu\forms\menu\MenuForm */
+/* @var $menu abdualiym\menu\entities\Menu */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
 <div class="menu-form">
     <?php $form = activeform::begin(); ?>
+    <div class="box box-default">
+        <div class="box-header with-border">Главная</div>
+        <div class="box-body">
+            <?= $form->field($model, 'status')->label('Статус')->dropDownList([Menu::VISIBLE => "Активный", Menu::HIDDEN => "Не активный"]) ?>
+            <?= $form->field($model, 'type')->label('Тип меню')->dropDownList($menuTypes) ?>
+            <div class="menu_types" style="display: none;"></div>
+            <?= $form->field($model, 'parentId')
+                ->label("Родительское меню")
+                ->dropDownList(
+                    $model->parentMenuList()
+                );
+            ?>
+        </div>
+    </div>
+
     <div class="box box-default">
         <div class="box-header with-border">Заголовки по всем языкам</div>
         <div class="box-body">
@@ -36,11 +52,11 @@ if (!empty($menu)) {
             <!-- Nav tabs -->
             <ul class="nav nav-tabs" role="tablist">
                 <?php for ($i = 0; $i < count($langList); $i++): ?>
-                <li role="presentation" <?= $i == 0 ? 'class="active"' : '' ?>>
-                    <a href="#<?= $langList[$i]['prefix'] ?>" aria-controls="<?= $langList[$i]['prefix'] ?>" role="tab" data-toggle="tab">
-                        <?= '(' . $langList[$i]['prefix'] . ') ' . $langList[$i]['title'] ?>
-                    </a>
-                </li>
+                    <li role="presentation" <?= $i == 0 ? 'class="active"' : '' ?>>
+                        <a href="#<?= $langList[$i]['prefix'] ?>" aria-controls="<?= $langList[$i]['prefix'] ?>" role="tab" data-toggle="tab">
+                            <?= '(' . $langList[$i]['prefix'] . ') ' . $langList[$i]['title'] ?>
+                        </a>
+                    </li>
                 <?php endfor ?>
             </ul>
 
@@ -57,25 +73,6 @@ if (!empty($menu)) {
         </div>
     </div>
 
-
-
-
-    <div class="box box-default">
-        <div class="box-header with-border">Главная</div>
-        <div class="box-body">
-            <?= $form->field($model, 'status')->label('Статус')->dropDownList([Menu::VISIBLE => "Активный", Menu::HIDDEN => "Не активный"]) ?>
-            <?= $form->field($model, 'type')->label('Тип меню')->dropDownList($menuTypes,['prompt'=>'Выберите...']) ?>
-            <div class="menu_types" style="display: none;"></div>
-            <?= $form->field($model, 'parentId')
-                ->label("Родительское меню")
-                ->dropDownList(
-                    $model->parentMenuList()
-                );
-            ?>
-        </div>
-    </div>
-
-
     <div class="form-group">
         <?= Html::submitButton("Создать", ['class' => 'btn btn-success']) ?>
     </div>
@@ -86,37 +83,41 @@ if (!empty($menu)) {
 </div>
 
 <?php
+
 $menu = new Menu();
 $actionsList = $menu->actionsList();
 $categoriesListJson = Json::encode($categoriesList);
 $textsListJson = Json::encode($textsList);
 $actionsListJson = Json::encode($actionsList);
 $script = <<< JS
-
-    function getHtml(currentType,categoriesList,textsList,actionsList){
+    function getHtml(currentType,categoriesList,textsList,actionsList, selected = false){
     var categoryKeys = Object.keys(categoriesList);
     var textKeys = Object.keys(textsList);
     var actionsKeys = Object.keys(actionsList);
     var optionsCat = '';
     var optionsText = '';
     var optionsAction = '';
+    var selectedAttr = '';
     for(var i=0; i < categoryKeys.length; i++)
         {
-            optionsCat += '<option value="'+categoryKeys[i]+'">'+categoriesList[categoryKeys[i]]+'</option>';
+            selectedAttr = selected == categoryKeys[i] ? ' selected="selected"' : '';
+            optionsCat += '<option '+selectedAttr+' value="'+categoryKeys[i]+'">'+categoriesList[categoryKeys[i]]+'</option>';
         }
     for(var i=0; i < textKeys.length; i++)
         {
-            optionsText += '<option value="'+textKeys[i]+'">'+textsList[textKeys[i]]+'</option>';
+            selectedAttr = selected == textKeys[i] ? ' selected="selected"' : '';
+            optionsText += '<option '+selectedAttr+' value="'+textKeys[i]+'">'+textsList[textKeys[i]]+'</option>';
         }
     for(var i=0; i < actionsKeys.length; i++)
         {
-            optionsAction += '<option value="'+actionsKeys[i]+'">'+actionsList[actionsKeys[i]]+'</option>';
+            selectedAttr = selected == actionsKeys[i] ? ' selected="selected"' : '';
+            optionsAction += '<option '+selectedAttr+' value="'+actionsKeys[i]+'">'+actionsList[actionsKeys[i]]+'</option>';
         }
     if(!currentType){return ''};
             switch (currentType){
                 case 'link':
                     return '<label class="control-label" for="menuform-type_helper">Введите ссылку</label>'
-                            +'<input type="text" id="menuform-type_helper" class="form-control" name="MenuForm[type_helper]" maxlength="255" placeholder="http://">';
+                            +'<input type="text" id="menuform-type_helper" class="form-control" name="MenuForm[type_helper]" maxlength="255" placeholder="http://" value="'+selected+'">';
                            
                 case 'category':
                     return '<label class="control-label" for="menuform-type_helper">Выберите категорию контента</label>'
@@ -136,31 +137,34 @@ $script = <<< JS
             }
     }
     
+    function render(val){
+        menu_types.html('<div class="form-group field-menuform-type_helper">'
+                       +getHtml(val,categoriesList,textsList,actionsList,typeHelper)
+                       +'<div class="help-block"></div></div>'                        
+                       );
+       menu_types.hide(0);
+       menu_types.slideDown(150);
+    }
     
     var type = $("#menuform-type"),
         menu_types = $(".menu_types"),
         categoriesList = $categoriesListJson,
         textsList = $textsListJson,
         actionsList = $actionsListJson;
-    
+        typeHelper = '$typeHelper';
+        
+    if(typeHelper){
+       render(type.val());
+    }
         
     type.on('change',function() {
-        menu_types.html('<div class="form-group field-menuform-type_helper">'
-                        +getHtml($(this).val(),categoriesList,textsList,actionsList)
-                        +'<div class="help-block"></div></div>'
-                        
-                        
-                        );
-        menu_types.hide(0);
-        menu_types.slideDown(200);
-    }); 
-        
-        
-        
-        
+       render($(this).val());
+    });
 
 JS;
+
 $this->registerJs(
     $script
 );
+
 ?>
--- views/menu/view.php
+++ views/menu/view.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use abdualiym\languageClass\Language;
+use abdualiym\menu\entities\Menu;
+use abdualiym\text\entities\CategoryTranslation;
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\Menu\entities\Menu */
@@ -26,13 +29,86 @@ $this->params['breadcrumbs'][] = $this->title;
         'model' => $model,
         'attributes' => [
             'id',
-            'status',
-            'type',
-            'type_helper',
-            'created_at',
-            'created_by',
-            'updated_at',
-            'updated_by',
+            [
+                'attribute' => 'status',
+                'filter' => [Menu::VISIBLE => 'Активные', Menu::HIDDEN => 'Не активные'],
+                'value' => function ($model) {
+                    return $model->status == 1 ? 'Активный' : 'Не активный';
+                }
+            ],
+            [
+                'attribute' => 'parent',
+                'label' => 'Родительское меню',
+                'value' => function ($model) {
+                    $parent = $model->getParent()->with('translate')->one();
+
+                    if ($parent) {
+                        foreach ($parent->translate as $translation){
+                            if($translation->lang_id == (Language::getLangByPrefix('ru'))['id']){
+                                $translate = $translation;
+                            }
+                        }
+                        $parent = Html::a(Html::encode($translate->title), ['view', 'id' => $parent->id]);
+                    } else {
+                        $parent = 'Основное';
+                    }
+                    return $parent;
+                },
+                'format' => 'raw'
+
+            ],
+            [
+                'attribute' => 'type',
+                'filter' => Menu::getMenuTypes(),
+                'value' => function($model){
+                    $types = Menu::getMenuTypes();
+                    return $types[$model->type];
+                }
+            ],
+            [
+                'attribute' => 'type_helper',
+                'label' => 'Перейти',
+                'value' => function($model){
+                    switch ($model->type){
+                        case 'link':
+                            return Html::a('Перейти по ссылке ',$model->type_helper);
+                        case 'category':
+                            $cats = CategoryTranslation::find()
+                                ->where(['parent_id' => $model->type_helper])
+                                ->asArray()
+                                ->all();
+                            foreach (Language::langList(
+                                Yii::$app->params['languages'],
+                                true) as $lang){
+                                $arr[] = Html::a($lang['title'],
+                                    Yii::$app->params['frontendHostInfo'].'/'.$lang['prefix']
+                                    .Menu::getSlug(
+                                        $cats[0]['slug'],
+                                        $model->type,$model->type_helper,$lang));
+                            }
+
+                            return implode('<br/>',$arr);
+                    }
+                },
+                'format' => 'html'
+            ],
+            'type_helper:html',
+            'created_at:datetime',
+            'updated_at:datetime',
+            [
+                'attribute' => 'created_by',
+                'label' => 'Создал',
+                'value' => function($model){
+                    return (\backend\entities\User::findOne($model->created_by))->username;
+                }
+            ],
+            [
+                'attribute' => 'updated_by',
+                'label' => 'Обнавил',
+                'value' => function($model){
+                    return (\backend\entities\User::findOne($model->created_by))->username;
+                }
+            ],
         ],
     ]) ?>
 
