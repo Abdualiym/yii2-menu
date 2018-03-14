531624e384b53b9c68b83df03a58aacff9852e2a
TermiT <web4@ivc.tshtt.uz>
2018-03-14 19:11:15 +0500

    fixed

--- views/menu/_form.php
+++ views/menu/_form.php
@@ -87,7 +87,7 @@ if (!empty($menu)) {
 
 
 $menu = new Menu();
-$actionsList = $menu->modulesList();
+$actionsList = $menu->actionsList();
 $categoriesListJson = Json::encode($categoriesList);
 $textsListJson = Json::encode($textsList);
 $actionsListJson = Json::encode($actionsList);
